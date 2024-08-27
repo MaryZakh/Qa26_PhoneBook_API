701b4740ca2f6c1b2b912e96d5aeb983c7c14ae5
Maria Z <z0559882272@gmail.com>
2024-08-27 20:31:47 +0300

    login class

---
+++ src/main/java/dto/ErrorDTO.java
@@ -0,0 +1,28 @@
+package dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+
+public class ErrorDTO {
+
+//    {
+//
+//            "status": 0,
+//            "error": "string",
+//            "message": {},
+//        "path": "string"
+//    }
+
+    private int status;
+    private String error;
+    private Object message;
+    private String path;
+
+}
